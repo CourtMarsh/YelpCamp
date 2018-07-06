2744e9e2be7b5337f7fb556332821cfa61fd8e21
Court <courtmarsh29@gmail.com>
2018-07-06 00:05:21 +0000

    Update mongoose.connect for production

--- app.js
+++ app.js
@@ -20,8 +20,10 @@ var     commentRoutes    = require("./routes/comments"),
         campgroundRoutes = require("./routes/campgrounds"),
         indexRoutes      = require("./routes/index")
 
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
-mongoose.connect("url");
+mongoose.connect("mongodb://Court:hookem4life@ds139438.mlab.com:39438/courtsyelpcamp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+//var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+//mongoose.connect("url");
 
 
 app.use(bodyParser.urlencoded({extended: true}));
